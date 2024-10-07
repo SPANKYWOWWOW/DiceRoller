a77581083a6d4b922e53418bad18b3280150b096
Apple develper tutorials <pavandavar99@gmail.com>
2024-10-07 10:45:37 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Pavan Davar on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRollerTests/DiceRollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerTests.swift
+//  DiceRollerTests
+//
+//  Created by Pavan Davar on 10/7/24.
+//
+
+import Testing
+@testable import DiceRoller
+
+struct DiceRollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
